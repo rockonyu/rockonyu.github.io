e866b94e29abcbf870f8d5bda77a35be18a08ee4
yuzhang <rockonyu@gmail.com>
2020-06-21 14:01:04 +0800

    chore: move resume logic into components

--- src/components/resume/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Contact } from "./Contact"
-export { default as Main } from "./Main"
-export { default as MileStone } from "./MileStone"
-export { default as SkillTree } from "./SkillTree"
-export { default as resume } from "./resume.json"
