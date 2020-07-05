5e15b6b99c30160bc336a8d6fbf58fb7bbbd4abc
yuzhang <rockonyu@gmail.com>
2020-07-05 18:20:03 +0800

    feat: 調整字型

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -5,4 +5,11 @@ export default {
   styles: {
     ...theme.styles,
   },
+
+  fonts: {
+    body:
+      '-apple-system,BlinkMacSystemFont,"Helvetica Neue",Arial,微軟正黑體,sans-serif',
+    heading:
+      '-apple-system,BlinkMacSystemFont,"Helvetica Neue",Arial,微軟正黑體,sans-serif',
+  },
 }
