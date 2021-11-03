1dded32ec3e553785e77b871e1aa6638ee0b6f49
austin <rockonyu@gmail.com>
2021-11-03 11:14:00 +0800

    feat: replace remark with unified

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -1,27 +1,17 @@
 import markdownStyles from './markdown-styles.module.css'
-import { useEffect } from 'react'
-
-import hljs from 'highlight.js'
 import 'highlight.js/styles/github.css'
 
 type Props = {
   content: string
 }
 
-const PostBody = ({ content }: Props) => {
-  useEffect(() => {
-    document.querySelectorAll<HTMLElement>('pre code').forEach((block) => {
-      hljs.highlightElement(block)
-    })
-  }, [])
-  return (
-    <div className="max-w-2xl mx-auto">
-      <div
-        className={markdownStyles['markdown']}
-        dangerouslySetInnerHTML={{ __html: content }}
-      />
-    </div>
-  )
-}
+const PostBody = ({ content }: Props) => (
+  <div className="max-w-2xl mx-auto">
+    <div
+      className={markdownStyles['markdown']}
+      dangerouslySetInnerHTML={{ __html: content }}
+    />
+  </div>
+)
 
 export default PostBody
--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,7 +1,19 @@
-import { remark } from 'remark'
-import html from 'remark-html'
+import remarkParse from 'remark-parse'
+import remarkRehype from 'remark-rehype'
+import rehypeHighlight from 'rehype-highlight'
+import rehypeStringify from 'rehype-stringify'
+import { unified } from 'unified'
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown)
+  const result = await unified()
+    // parses markdown to mdast syntax trees.
+    .use(remarkParse)
+    // remark plugin to bridge or mutate to rehype.
+    .use(remarkRehype)
+    // rehype plugin to highlight the syntax of code with lowlight
+    .use(rehypeHighlight)
+    // rehype plugin to serialize HTML, compiler for unified.
+    .use(rehypeStringify)
+    .process(markdown)
   return result.toString()
 }
