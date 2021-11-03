88620cece995c1d1a4a0828200409b65d4a08740
austin <rockonyu@gmail.com>
2021-11-03 15:24:49 +0800

    feat: use tsconfig paths

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
-import { getAllPosts } from '../lib/api'
+import Container from 'components/container'
+import MoreStories from 'components/more-stories'
+import HeroPost from 'components/hero-post'
+import Intro from 'components/intro'
+import Layout from 'components/layout'
+import { getAllPosts } from 'lib/api'
 import Head from 'next/head'
-import Post from '../types/post'
+import Post from 'types/post'
 
 type Props = {
   allPosts: Post[]
