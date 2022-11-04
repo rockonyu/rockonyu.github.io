0bd59121a5b6353c1400daefb305fbed53b4d79f
austin <rockonyu@gmail.com>
2022-11-04 13:13:25 +0800

    feat: footer quotes

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,28 +1,41 @@
+import { useEffect, useState } from 'react'
 import Container from './Container'
-import { EXAMPLE_PATH } from '../../lib/constants'
+
+const QUOTES_URL =
+  'https://raw.githubusercontent.com/daviferreira/defprogramming.com/master/src/data/quotes.json'
 
 const Footer = () => {
+  const [quote, setQuote] = useState<{
+    body: string
+    authors: string[]
+  }>()
+
+  useEffect(() => {
+    const fetchQuotes = async () => {
+      const quotes = await (await fetch(QUOTES_URL)).json()
+
+      const randomIndex = Math.min(
+        Math.floor(Math.random() * quotes.length),
+        quotes.length - 1,
+      )
+      setQuote(quotes[randomIndex])
+    }
+
+    fetchQuotes()
+  }, [])
+
   return (
     <footer className="bg-accent-1 border-t border-accent-2 dark:bg-accent-8">
       <Container>
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4">
-            Statically Generated with Next.js.
+            {quote && (
+              <>
+                "{quote.body}"
+                <br />- {quote.authors.join(', ')}
+              </>
+            )}
           </h3>
-          {/* <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://nextjs.org/docs/basic-features/pages"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
-            <a
-              href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
-            </a>
-          </div> */}
         </div>
       </Container>
     </footer>
